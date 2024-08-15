5ced86e9c44bbee8ecdcedad521dfd55691acb39
zer0int <132047210+zer0int@users.noreply.github.com>
2024-08-15 13:44:31 +0200

    Add support for Flux

--- __init__.py
+++ __init__.py
@@ -3,9 +3,11 @@ from . import long_clip as long_clip
 NODE_CLASS_MAPPINGS = {
     "SeaArtLongClip": long_clip.SeaArtLongClip,
     "SeaArtLongXLClipMerge": long_clip.SeaArtLongXLClipMerge,
+    "LongCLIPTextEncodeFlux": long_clip.LongCLIPTextEncodeFlux,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "SeaArtLongClip": "SeaArtLongClip",
     "SeaArtLongXLClipMerge": "SeaArtLongXLClipMerge",
+    "LongCLIPTextEncodeFlux": "LongCLIPTextEncodeFlux",
 }
